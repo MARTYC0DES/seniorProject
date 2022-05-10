ee24df4746c3ecb3fd661db428e21f916fc6a3c9
Martin-Dom <63745768+MARTYC0DES@users.noreply.github.com>
2022-05-10 13:07:57 -0700

    Update inquiry-form.php
    Fixed error on line 12

--- inquiry-form.php
+++ inquiry-form.php
@@ -7,19 +7,21 @@
 
     $email_subject = "New Inquiry Submission";
 
-    $email_body = "Student's First Name: $firstName. \n".
-                    "Student's Last Name $lastName. \n"
-                        "Student's Email: $email. \n".
-                            "Student's bio: $about. \n".
-                                "Game student is interested in: $game. \n";
+    $email_from ='sacstateesportsclub@sacstate.com';
 
-    //Email you would like to be the inquiry sent to
+    $email_body = "Student's First Name: $firstName.\n".
+                    "Student's Last Name: $lastName.\n"
+                        "Student's Email: $email.\n".
+                            "Student's bio: $about.\n".
+                                "Game student is interested in: $game.\n";
+
+    //Email you would like to receive the inquiry
     $to = "derrickmar03@gmail.com";
 
-    $headers = "From: $email \r\n";
+    $headers = "From: $email_from \r\n";
     $headers .= "Reply-To: $email \r\n";
 
-    mail($to, $email_subject, $email_body, $headers);
+    mail($to,$email_subject,$email_body,$headers);
 
     header("Location: index.html");
 
